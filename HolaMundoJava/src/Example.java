253cef9ed131f0618673dfdb827a9057888d6ee3
ajrianop <ajrianop@unal.edu.co>
2024-04-30 23:42:50 -0500

    new functions

---
+++ HolaMundoJava/src/Example.java
@@ -0,0 +1,7 @@
+class Example {
+    public static void main(String[] args){
+        int age = 21;
+
+        System.out.println("I am " + age + " years old");
+    }
+}
---
+++ HolaMundoJava/src/Types_of_data.java
@@ -0,0 +1,23 @@
+class Types_of_data {
+    public static void main(String[] args) {
+        // Integers types
+        byte aSingleByte = 100;
+        short aSmallNumber = 20000;
+        int anInteger = 1234324;
+        long aLargerNumber = 2093833232;
+        
+        // decimal types
+        double aDouble = 1.892;
+        float aFloat = 3.234F;
+
+        // Booleans
+        boolean isWeekend = false;
+        boolean isWorkday = true;
+        
+        // characters
+        char copyrightSymbol = '\u00A9';
+
+        System.out.println("This is the copyright symbol: " + copyrightSymbol);
+
+    }
+}
