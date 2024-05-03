813b8f87fde4848b2620503eeb74361d493caed2
ajrianop <ajrianop@unal.edu.co>
2024-05-02 21:49:04 -0500

    Operations between numbers

---
+++ HolaMundoJava/src/ChangeNumberType.java
@@ -0,0 +1,10 @@
+class ChangeNumberType {
+    public static void main(String[] args) {
+        int num1 = 5;
+        double num2 = num1;
+        System.out.println(num2);
+        double num3 = 45.3;
+        int num4 = (int) num3;
+        System.out.println(num4);
+    }    
+}
---
+++ HolaMundoJava/src/Operators.java
@@ -0,0 +1,21 @@
+class Operators {
+    public static void main(String[] args) {
+        int num1 = 25;
+        int num2 = 36;
+
+        //addition
+        System.out.println(num1 + num2);
+
+        // Substraction
+        System.out.println(num1 - num2);
+
+        // Multiplication
+        System.out.println(num1 * num2);
+
+        // Division
+        System.out.println(num1 / num2);
+        
+        // Remainder
+        System.out.println(num1 % num2);
+    }
+}
