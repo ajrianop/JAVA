3a3f0e90d13a9e6d6397e1ace53ac14f92e46c06
ajrianop <ajrianop@unal.edu.co>
2024-05-07 23:48:47 -0500

    Incremental and Decremental value

---
+++ HolaMundoJava/src/IncrementalDecrementalValues.java
@@ -0,0 +1,16 @@
+class IncrementalDecrementalValues {
+
+    public static void main(String[] args) {
+        int step = 1;
+        int floor = 21;
+
+        //Increasing one value for step
+        step++;
+        // Decreasing one value for floor
+        floor--;
+
+        System.out.println(step);
+        System.out.println(floor);
+
+    }
+}
