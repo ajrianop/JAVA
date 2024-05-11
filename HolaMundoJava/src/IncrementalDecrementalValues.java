974c16f38e68f2c6d7c75a00a9853e9bfcdf9d06
ajrianop <ajrianop@unal.edu.co>
2024-05-10 21:27:13 -0500

    incremental and decremental operators, different uses

--- HolaMundoJava/src/IncrementalDecrementalValues.java
+++ HolaMundoJava/src/IncrementalDecrementalValues.java
@@ -12,5 +12,15 @@ class IncrementalDecrementalValues {
         System.out.println(step);
         System.out.println(floor);
 
+        // See that when we print inmediately the value using the increasing by means ++
+        // We obtain the first value saved, if we print again the variable, this appears with a plus 1
+        int number = 10;
+        System.out.println(number++);
+        System.out.println(number);
+
+        // We can use the incresing (decreasing) operator adding ++ (or --) before the variable 
+        int number2 = 100;
+        System.out.println(--number2);
+        System.out.println(number2);
     }
 }
