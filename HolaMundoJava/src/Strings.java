cd10d364685b1c11f3d3584926449e94591c3019
ajrianop <ajrianop@unal.edu.co>
2024-05-15 21:05:04 -0500

    working with strings

---
+++ HolaMundoJava/src/Strings.java
@@ -0,0 +1,8 @@
+public class Strings {
+    public static void main(String[] args) {
+        String name = "Karl Wilham";
+        String country = "Bangladesh";
+        int age = 21;
+        System.out.println("Hi user, my is " + name + ". I am from " + country + " and I am " + age + " years old.");
+    }
+}
