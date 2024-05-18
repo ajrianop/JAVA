31e9cca0df6a9f15e2b49418c22bc8cfd284b507
ajrianop <ajrianop@unal.edu.co>
2024-05-17 21:30:31 -0500

    Adding double to string formatting

--- HolaMundoJava/src/StringFormat.java
+++ HolaMundoJava/src/StringFormat.java
@@ -3,8 +3,9 @@ public class StringFormat {
         String day = "Monday";
         int number_day = 16;
         String month = "June";
+        double temperature = 12.4;
         
-        String formattingString = String.format("Today is %s %d of %s", day, number_day, month);
+        String formattingString = String.format("Today is %s %d of %s, with temperature %f C", day, number_day, month, temperature);
 
         System.out.println(formattingString);
 
