4a6ffe23b6e2e13e1d9fc09a3d7f0fe20410e9d6
ajrianop <ajrianop@unal.edu.co>
2024-05-22 21:58:22 -0500

    Adding String methods

---
+++ HolaMundoJava/src/StringMethods.java
@@ -0,0 +1,12 @@
+public class StringMethods {
+    public static void main(String[] args) {
+        String soccer_team = "Borussia Dormunt";
+
+        System.out.println("The lenght is: ");
+        System.out.println(soccer_team.length());
+        System.out.println(soccer_team.isEmpty());
+        System.out.println(soccer_team.toUpperCase());
+        System.out.println(soccer_team.toLowerCase());
+        System.out.println(soccer_team);
+    }
+}
