3d3feca86ffd886eeb60a76bc7f8caa5dcbbe790
ajrianop <ajrianop@unal.edu.co>
2024-05-29 21:54:28 -0500

    String method contains

---
+++ HolaMundoJava/src/ContainingInStrings.java
@@ -0,0 +1,8 @@
+public class ContainingInStrings {
+    public static void main(String[] args) {
+        String str1 = "The idea is to save all the data";
+
+        // To see if in the string exists any word in it, we can use the method contains
+        System.out.println(str1.contains("data"));
+    }
+}
